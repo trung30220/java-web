a6a22756292ef98f5c939a34748b0c7f4d1bf178
trung30220 <trung30220@gmail.com>
2021-01-23 14:25:29 +0700

    fix

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Nguyen Duc Trung HELLO today 2";
+        return "Nguyen Duc Trung HELLO today 3";
     }
 }
